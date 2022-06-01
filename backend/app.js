16d4fc146c7a705a2b6110ed984326883cac00bb
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-06-01 12:29:15 +0800

    node server working

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const cheerio = require("cheerio");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+app.get("/", (req, res)=> {
+    res.json({"message" : "working"})
+})
+
+app.get("api/search/:company", ()=> {
+    const company = req.query.company;
+    fetch(`https://www.societe.com/cgi-bin/search?champs=${company}`)
+})
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
