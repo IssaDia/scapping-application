ad4aa500137a4da9cab20f738e51eab05005876b
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-06-05 09:11:20 +0800

    refacto on front and back

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 
-const cheerio = require("cheerio");
-const axios = require("axios");
-
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const {
+  getCompanyDataFromWebsite,
+} = require("./utils/getCompanyDataFromWebsite");
 
 const port = 3000;
 
@@ -22,31 +23,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/search/:company", async (req, res) => {
-  const company = req.params.company;
-  const URL = `https://www.societe.com/cgi-bin/search?champs=${company}`;
-
-  axios
-    .get(URL)
-    .then(async (response) => {
-      const $ = await cheerio.load(response.data);
-      const targetedElm = $(
-        "div#result_doc > .ResultBloc__link > div > .ResultBloc__link__content .extract"
-      ).first();
-
-      const name = targetedElm
-        .find("p.txt > span.highlight")
-        .first()
-        .text()
-        .trim();
-      const siren = targetedElm
-        .find("p.txt")
-        .first()
-        .text()
-        .match(/[0-9]{3} [0-9]{3} [0-9]{3}/);
-
-      res.json({ name, siren });
-    })
-    .catch((error) => console.log(error));
+  getCompanyDataFromWebsite(req, res);
 });
 
 app.listen(port, () => {
---
+++ backend/utils/getCompanyDataFromWebsite.js
@@ -0,0 +1,32 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+const getCompanyDataFromWebsite = (req, res) => {
+  const company = req.params.company;
+  const URL = `https://www.societe.com/cgi-bin/search?champs=${company}`;
+
+  axios
+    .get(URL)
+    .then(async (response) => {
+      const $ = await cheerio.load(response.data);
+      const targetedElm = $(
+        "div#result_doc > .ResultBloc__link > div > .ResultBloc__link__content .extract"
+      ).first();
+
+      const name = targetedElm
+        .find("p.txt > span.highlight")
+        .first()
+        .text()
+        .trim();
+      const siren = targetedElm
+        .find("p.txt")
+        .first()
+        .text()
+        .match(/[0-9]{3} [0-9]{3} [0-9]{3}/);
+
+      return res.json({ name, siren });
+    })
+    .catch((error) => console.log(error));
+};
+
+module.exports = { getCompanyDataFromWebsite };
